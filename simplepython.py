c0d4575eb8ba7acf2e81d4b586fd09f400956dd0
megha2124 <panchamukhimegha@gmail.com>
2026-07-15 15:19:33 +0530

    test

--- simplepython.py
+++ simplepython.py
@@ -3,7 +3,7 @@ print("🚀 Welcome to My First Docker App!")
 print("=" * 40)
 #learning git
 name = input("Enter your name: ")
-
+#learning git
 print(f"\nHello, {name}!")
 print("This Python application is running inside a Docker container.")
 print("Congratulations! 🎉")
